45f8ead9bd2e5bc127e3cc8a2f085263ed9e6777
Vitor Leone Cezar <vleone.social@gmail.com>
2021-08-12 21:17:46 -0300

    código da página de produtos, exibição de produtos com link

--- src/Components/Produtos.js
+++ src/Components/Produtos.js
@@ -1,8 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import useFetch from "../Hooks/useFetch";
 import styles from "./Produtos.module.css";
 
 const Produtos = () => {
-  return <section className={styles.produtos}></section>;
+  const { data, error, loading } = useFetch(
+    `https://ranekapi.origamid.dev/json/api/produto`
+  );
+
+  if (error) return <p>{error}</p>;
+  if (!data) return null;
+  return (
+    <section className={styles.produtos}>
+      <ul>
+        {data.map((produto) => (
+          <li key={produto.id}>
+            <Link to={`produto/${produto.id}`}>
+              <img src={produto.fotos[0].src} alt={produto.fotos[0].title} />
+              <h2>{produto.nome}</h2>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 };
 
 export default Produtos;
